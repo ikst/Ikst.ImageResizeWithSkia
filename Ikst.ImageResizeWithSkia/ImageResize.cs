611298c4b0bce6e3a4d3e8471d5647b5e237594f
ikst <oihsot.15@gmail.com>
2022-09-23 21:54:39 +0900

    .net standard基準でコード修正

--- Ikst.ImageResizeWithSkia/ImageResize.cs
+++ Ikst.ImageResizeWithSkia/ImageResize.cs
@@ -9,23 +9,34 @@ using System.Threading.Tasks;
 
 namespace Ikst.ImageResizeWithSkia
 {
+
+    /// <summary>
+    /// 画像変換
+    /// </summary>
     public class ImageResize
     {
 
+        /// <see cref="Resize(string, int, int, ResizeMode, SKColor, SKEncodedImageFormat, int, SKFilterQuality)"/>
         public static byte[] Resize(string filePath, int width, int height, ResizeMode mode)
         {
-            using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            return Resize(fs, width, height, mode);
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                return Resize(fs, width, height, mode);
+            }
         }
         public static byte[] Resize(byte[] bytes, int width, int height, ResizeMode mode)
         {
-            using MemoryStream ms = new MemoryStream(bytes);
-            return Resize(ms, width, height, mode);
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                return Resize(ms, width, height, mode);
+            }
         }
         public static byte[] Resize(Stream st, int width, int height, ResizeMode mode)
         {
-            using SKCodec sKCodec = SKCodec.Create(st);
-            return Resize(sKCodec, width, height, mode);
+            using (SKCodec sKCodec = SKCodec.Create(st))
+            {
+                return Resize(sKCodec, width, height, mode);
+            }
         }
         public static byte[] Resize(SKCodec sKCodec, int width, int height, ResizeMode mode)
         {
@@ -38,18 +49,24 @@ namespace Ikst.ImageResizeWithSkia
 
         public static byte[] Resize(string filePath, int width, int height, ResizeMode mode, SKColor backColor)
         {
-            using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            return Resize(fs, width, height, mode, backColor);
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                return Resize(fs, width, height, mode, backColor);
+            }
         }
         public static byte[] Resize(byte[] bytes, int width, int height, ResizeMode mode, SKColor backColor)
         {
-            using MemoryStream ms = new MemoryStream(bytes);
-            return Resize(ms, width, height, mode, backColor);
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                return Resize(ms, width, height, mode, backColor);
+            }
         }
         public static byte[] Resize(Stream st, int width, int height, ResizeMode mode, SKColor backColor)
         {
-            using SKCodec sKCodec = SKCodec.Create(st);
-            return Resize(sKCodec, width, height, mode, backColor);
+            using (SKCodec sKCodec = SKCodec.Create(st))
+            {
+                return Resize(sKCodec, width, height, mode, backColor);
+            }
         }
         public static byte[] Resize(SKCodec sKCodec, int width, int height, ResizeMode mode, SKColor backColor)
         {
@@ -60,18 +77,24 @@ namespace Ikst.ImageResizeWithSkia
 
         public static byte[] Resize(string filePath, int width, int height, ResizeMode mode, SKEncodedImageFormat fmt)
         {
-            using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            return Resize(fs, width, height, mode, fmt);
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                return Resize(fs, width, height, mode, fmt);
+            }
         }
         public static byte[] Resize(byte[] bytes, int width, int height, ResizeMode mode, SKEncodedImageFormat fmt)
         {
-            using MemoryStream ms = new MemoryStream(bytes);
-            return Resize(ms, width, height, mode, fmt);
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                return Resize(ms, width, height, mode, fmt);
+            }
         }
         public static byte[] Resize(Stream st, int width, int height, ResizeMode mode, SKEncodedImageFormat fmt)
         {
-            using SKCodec sKCodec = SKCodec.Create(st);
-            return Resize(sKCodec, width, height, mode, fmt);
+            using (SKCodec sKCodec = SKCodec.Create(st))
+            {
+                return Resize(sKCodec, width, height, mode, fmt);
+            }
         }
         public static byte[] Resize(SKCodec sKCodec, int width, int height, ResizeMode mode, SKEncodedImageFormat fmt)
         {
@@ -82,18 +105,24 @@ namespace Ikst.ImageResizeWithSkia
 
         public static byte[] Resize(string filePath, int width, int height, ResizeMode mode, SKColor backColor, SKEncodedImageFormat fmt, int quality = 90, SKFilterQuality fq = SKFilterQuality.Medium)
         {
-            using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            return Resize(fs, width, height, mode, backColor, fmt, quality, fq);
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                return Resize(fs, width, height, mode, backColor, fmt, quality, fq);
+            }
         }
         public static byte[] Resize(byte[] bytes, int width, int height, ResizeMode mode, SKColor backColor, SKEncodedImageFormat fmt, int quality = 90, SKFilterQuality fq = SKFilterQuality.Medium)
         {
-            using MemoryStream ms = new MemoryStream(bytes);
-            return Resize(ms, width, height, mode, backColor, fmt, quality, fq);
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                return Resize(ms, width, height, mode, backColor, fmt, quality, fq);
+            }
         }
         public static byte[] Resize(Stream st, int width, int height, ResizeMode mode, SKColor backColor, SKEncodedImageFormat fmt, int quality = 90, SKFilterQuality fq = SKFilterQuality.Medium)
         {
-            using SKCodec sKCodec = SKCodec.Create(st);
-            return Resize(sKCodec, width, height, mode, backColor, fmt, quality, fq);
+            using (SKCodec sKCodec = SKCodec.Create(st))
+            {
+                return Resize(sKCodec, width, height, mode, backColor, fmt, quality, fq);
+            }
         }
 
 
@@ -112,46 +141,56 @@ namespace Ikst.ImageResizeWithSkia
         public static byte[] Resize(SKCodec sKCodec, int width, int height, ResizeMode mode, SKColor backColor, SKEncodedImageFormat imgFmt, int quality = 90, SKFilterQuality fq = SKFilterQuality.Medium)
         {
 
-            using SKBitmap srcBitmap = SKBitmap.Decode(sKCodec);
-
-            // width,heightのどちらかで0を指定された場合は、もう片方を基準に縦横比を維持したサイズとみなす
-            if (width == 0 || height == 0)
+            using (SKBitmap srcBitmap = SKBitmap.Decode(sKCodec))
             {
-                if (width == 0) width = (int)(height * ((float)srcBitmap.Width / (float)srcBitmap.Height));
-                if (height == 0) height = (int)(width * ((float)srcBitmap.Height / (float)srcBitmap.Width));
-                mode = ResizeMode.Default;
+
+                // width,heightのどちらかで0を指定された場合は、もう片方を基準に縦横比を維持したサイズとみなす
+                if (width == 0 || height == 0)
+                {
+                    if (width == 0) width = (int)(height * ((float)srcBitmap.Width / (float)srcBitmap.Height));
+                    if (height == 0) height = (int)(width * ((float)srcBitmap.Height / (float)srcBitmap.Width));
+                    mode = ResizeMode.Default;
+                }
+
+                SKImage ski;
+                if (mode == ResizeMode.Default)
+                {
+                    using (SKBitmap scaledBitmap = srcBitmap.Resize(new SKImageInfo(width, height), fq))
+                    {
+                        ski = SKImage.FromBitmap(scaledBitmap);
+                    }
+                }
+                else
+                {
+                    bool toFill = false;
+                    if (mode == ResizeMode.UniformToFill) toFill = true;
+
+                    var info = new SKImageInfo(width, height);
+                    using (SKSurface surface = SKSurface.Create(info))
+                    using (SKCanvas canvas = surface.Canvas)
+                    {
+
+                        canvas.Clear(backColor);
+
+                        var s = ConvertUniformSize(srcBitmap.Width, srcBitmap.Height, width, height, toFill);
+                        using (SKBitmap scaledBitmap = srcBitmap.Resize(new SKImageInfo((int)s.Width, (int)s.Height), fq))
+                        {
+
+                            float x = (width - s.Width) / 2;
+                            float y = (height - s.Height) / 2;
+                            canvas.DrawBitmap(scaledBitmap, x, y);
+
+                            ski = surface.Snapshot();
+                        }
+                    }
+                }
+
+                using (SKData data = ski.Encode(imgFmt, quality))
+                {
+                    ski.Dispose();
+                    return data.ToArray();
+                }
             }
-
-            SKImage ski;
-            if (mode == ResizeMode.Default)
-            {
-                using SKBitmap scaledBitmap = srcBitmap.Resize(new SKImageInfo(width, height), fq);
-                ski = SKImage.FromBitmap(scaledBitmap);
-            }
-            else
-            {
-                bool toFill = false;
-                if (mode == ResizeMode.UniformToFill) toFill = true;
-
-                var info = new SKImageInfo(width, height);
-                using SKSurface surface = SKSurface.Create(info);
-                using SKCanvas canvas = surface.Canvas;
-
-                canvas.Clear(backColor);
-
-                var s = ConvertUniformSize(srcBitmap.Width, srcBitmap.Height, width, height, toFill);
-                using SKBitmap scaledBitmap = srcBitmap.Resize(new SKImageInfo((int)s.Width, (int)s.Height), fq);
-
-                float x = (width - s.Width) / 2;
-                float y = (height - s.Height) / 2;
-                canvas.DrawBitmap(scaledBitmap, x, y);
-
-                ski = surface.Snapshot();
-            }
-
-            using SKData data = ski.Encode(imgFmt, quality);
-            ski.Dispose();
-            return data.ToArray();
 
             // SKImage.Encodeの画像フォーマットのデフォルトはpng
             // https://learn.microsoft.com/en-us/dotnet/api/skiasharp.skimage.encode?view=skiasharp-2.88
